785c0eff1946879b935f654f4579e0f1f77228e4
Jeremie GARCIA <jgarci4X>
2012-09-28 18:35:08 +0200

    [PORT FROM MAIN] wlan: Add improved interface logging from framework to wpa_supplicant
    BZ: 59886
    
    [DO NOT UPSTREAM]
    When upgrading to Jelly Bean, we've lost all wifi HAL command prints.
    This is due to a missing patch port:
       http://android.intel.com:8080/#/c/36209/
    
    However this patch has a 1024 char maximum print limitation.
    
    So this patch ports most of original one with little improvement.
    
    Change-Id: I65b52dd8dd65c997a32a79a206d514145ff18565
    Signed-off-by: Jeremie GARCIA <jeremiex.garcia@intel.com>
    Reviewed-on: http://android.intel.com:8080/78433
    Reviewed-by: Naoumenko, PaulX <paulx.naoumenko@intel.com>
    Reviewed-by: Trivelly, Jean <jean.trivelly@intel.com>
    Reviewed-by: Bachot, Jean-michel <jean-michel.bachot@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- wifi/wifi.c
+++ wifi/wifi.c
@@ -702,6 +702,31 @@ int wifi_connect_to_supplicant(const char *ifname)
     }
 }
 
+void log_cmd(const char *cmd)
+{
+    LOGI("CMD: %s\n", cmd);
+}
+
+void log_reply(char *reply, size_t *reply_len)
+{
+    char replyLocal[*reply_len];
+    char delims[] = "\n";
+    char *result = NULL;
+
+    strncpy(replyLocal, reply, *reply_len);
+
+    if (*reply_len > 0 && replyLocal[*reply_len-1] == '\n')
+        replyLocal[*reply_len-1] = '\0';
+    else
+        replyLocal[*reply_len] = '\0';
+
+    result = strtok(replyLocal , delims );
+    while( result != NULL ) {
+        LOGI("REPLY: %s\n", result);
+        result = strtok( NULL, delims );
+    }
+}
+
 int wifi_send_command(int index, const char *cmd, char *reply, size_t *reply_len)
 {
     int ret;
@@ -710,6 +735,7 @@ int wifi_send_command(int index, const char *cmd, char *reply, size_t *reply_len
         ALOGV("Not connected to wpa_supplicant - \"%s\" command dropped.\n", cmd);
         return -1;
     }
+    log_cmd(cmd);
     ret = wpa_ctrl_request(ctrl_conn[index], cmd, strlen(cmd), reply, reply_len, NULL);
     if (ret == -2) {
         ALOGD("'%s' command timed out.\n", cmd);
@@ -717,11 +743,13 @@ int wifi_send_command(int index, const char *cmd, char *reply, size_t *reply_len
         TEMP_FAILURE_RETRY(write(exit_sockets[index][0], "T", 1));
         return -2;
     } else if (ret < 0 || strncmp(reply, "FAIL", 4) == 0) {
+        LOGI("REPLY: FAIL\n");
         return -1;
     }
     if (strncmp(cmd, "PING", 4) == 0) {
         reply[*reply_len] = '\0';
     }
+    log_reply(reply, reply_len);
     return 0;
 }
 
@@ -811,7 +839,7 @@ int wifi_wait_on_socket(int index, char *buf, size_t buflen)
             memmove(buf, match+1, nread+1);
         }
     }
-
+    LOGI("EVENT: %s\n", buf);
     return nread;
 }
 
