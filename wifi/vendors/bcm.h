6d277df33cfeb740ba0b5bd3ef34f0313754b521
Quentin Casasnovas <quentinx.casasnovas@intel.com>
2013-07-08 18:20:26 +0200

    [PORT FROM MAIN]wifi: Add multi-vendor support
    BZ: 125764
    
    This patch adds an extra layer of indirection to call vendor specific code in
    the wifi HAL. The vendor specific code is called depending on
    wlan.driver.vendor property, itself defined by ueventd when it receives a
    specific modalias. Vendor specific code can also be disabled at compile time
    without compilation error as enums and other array of functions pointers are
    built at compile time.
    
    Category: device enablement
    Domain: CWS.WIFI-Common
    Origin: internal
    Upstream-Candidate: yes
    
    Change-Id: I4dd608050a2549055eb3d4dab0b1369b7b8304f4
    Orig-Change-Id: If430d833e64afea54f0f078558fffe4a196ae3f2
    Signed-off-by: Quentin Casasnovas <quentinx.casasnovas@intel.com>
    Reviewed-on: http://android.intel.com:8080/120542
    Reviewed-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

---
+++ wifi/vendors/bcm.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2008, The Android Open Source Project
+ * Copyright 2012-2013, Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BCM_H_
+# define BCM_H_
+
+#define WIFI_MODULE_43241_OPMODE        "/sys/module/bcm43241/parameters/op_mode"
+#define WIFI_MODULE_4334_OPMODE         "/sys/module/bcm4334/parameters/op_mode"
+#define WIFI_MODULE_4334X_OPMODE        "/sys/module/bcm4334x/parameters/op_mode"
+#define WIFI_MODULE_4335_OPMODE         "/sys/module/bcm4335/parameters/op_mode"
+
+int bcm_load_driver(void);
+int bcm_unload_driver(void);
+int bcm_is_driver_loaded(void);
+int bcm_switch_driver_mode(int mode);
+int bcm_change_fw_path(const char *fwpath);
+
+#endif /* !BCM_H_ */
