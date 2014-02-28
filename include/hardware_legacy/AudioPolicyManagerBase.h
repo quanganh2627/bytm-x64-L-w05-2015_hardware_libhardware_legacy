2bd9e421e02f2d48e54a30e76c592406e537c608
Vincent Becker <vincentx.becker@intel.com>
2014-02-28 12:18:56 +0100

    [AUDIO] Set isVirtualInputDevice method as protected
    BZ: 179042
    
    isVirtualInputDevice is needed by the derived audio policy,
    to check if the current input device is a virtual device.
    This methods is currently private to the base policy class,
    and can not be called from the daugther policy class.
    
    Set protected access to the isVirtualInputDevice method, so
    that it can be called from the derived policy.
    
    Change-Id: I623d4b88ce0de13092b949eff7814d161ecd85eb
    Category: device enablement
    Domain: AUDIO_MEDIA-WIDI
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: Vincent Becker <vincentx.becker@intel.com>

--- include/hardware_legacy/AudioPolicyManagerBase.h
+++ include/hardware_legacy/AudioPolicyManagerBase.h
@@ -611,6 +611,8 @@ protected:
         audio_io_handle_t mBGMOutput;
 
 
+        static bool isVirtualInputDevice(audio_devices_t device);
+
 private:
         void doSetFmParameters(AudioParameter& param);
         static float volIndexToAmpl(audio_devices_t device, const StreamDescriptor& streamDesc,
@@ -619,7 +621,6 @@ private:
         //    routing of notifications
         void handleNotificationRoutingForStream(AudioSystem::stream_type stream);
 
-        static bool isVirtualInputDevice(audio_devices_t device);
 #ifdef BGM_ENABLED
         bool IsRemoteBGMSupported(AudioSystem::stream_type stream);
         // return the strategy corresponding to a given stream type in case of BGM
